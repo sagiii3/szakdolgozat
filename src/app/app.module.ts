f70d4a0a6e739cda101ef8e72135afc2c50249e7
Solymosi Ágnes <solymosiagnes03@gmail.com>
2023-08-24 07:41:00 +0200

    firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+  firebase: {
+    projectId: 'szakdolgozat-alkalmazas',
+    appId: '1:1014090850949:web:c0221fcb3b1e2bb5607fec',
+    storageBucket: 'szakdolgozat-alkalmazas.appspot.com',
+    apiKey: 'AIzaSyAFELGzANS8EaXhS3Op2C425XrAK8wcHvw',
+    authDomain: 'szakdolgozat-alkalmazas.firebaseapp.com',
+    messagingSenderId: '1014090850949',
+  }
+      
+}
